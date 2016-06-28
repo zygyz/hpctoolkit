484ffbc7b398c788439788278d4c826d18db2374
John Mellor-Crummey <johnmc@rice.edu>
2016-06-28 16:18:32 -0500

    prototype linux perf now works for threads.

--- src/tool/hpcrun/sample-sources/linux_perf.c
+++ src/tool/hpcrun/sample-sources/linux_perf.c
@@ -42,7 +42,7 @@
 // ******************************************************* EndRiceCopyright *
 
 //
-// Linux perf sample source simple oo interface
+// Linux perf sample source interface
 //
 
 
@@ -51,21 +51,22 @@
  *****************************************************************************/
 
 #include <assert.h>
-#include <stdint.h>
-#include <signal.h>
-#include <stdio.h>        // printf
-#include <stdlib.h>       // getenv
-#include <sys/syscall.h>  // SYS_gettid
-#include <time.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #include <linux/perf_event.h>
+
+#include <sys/syscall.h> 
+#include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-
-#include <string.h>       // strlen
-#include <unistd.h>       // write
-#include <fcntl.h>
 
 
 
@@ -73,6 +74,8 @@
  * libmonitor
  *****************************************************************************/
 #include <monitor.h>
+
+
 
 /******************************************************************************
  * local includes
@@ -92,6 +95,7 @@
 #include <hpcrun/sample_sources_registered.h>
 #include <hpcrun/sample-sources/blame-shift/blame-shift.h>
 #include <hpcrun/utilities/tokenize.h>
+#include <hpcrun/utilities/arch/context-pc.h>
 
 #include <lib/prof-lean/hpcrun-fmt.h>
 
@@ -118,9 +122,21 @@
 
 #define DEFAULT_THRESHOLD  2000000L
 
-// FIXME: why are these not defined in Linux include files?
-#define rmb()
-#define mb()
+#define PERF_SIGNAL SIGIO
+
+#define PERF_DATA_PAGE_EXP        0      // use 2^PERF_DATA_PAGE_EXP pages
+#define PERF_DATA_PAGES           (1 << PERF_DATA_PAGE_EXP)  
+
+#define BUFFER_FRONT              ((char *) perf_mmap + pagesize)
+#define BUFFER_SIZE               (tail_mask + 1)
+#define BUFFER_OFFSET(tail)       ((tail) & tail_mask)
+
+#define PERF_MMAP_SIZE(pagesz)    ((pagesz) * (PERF_DATA_PAGES + 1)) 
+#define PERF_TAIL_MASK(pagesz)    (((pagesz) * PERF_DATA_PAGES) - 1) 
+
+#define PERF_EVENT_AVAILABLE_UNKNOWN 0
+#define PERF_EVENT_AVAILABLE_NO      1
+#define PERF_EVENT_AVAILABLE_YES     2
 
 
 
@@ -128,11 +144,14 @@
 // type declarations
 //******************************************************************************
 
-struct perf_event_callchain {
-	struct perf_event_header header;
-	uint64_t   nr;         /* number of IPs */ 
-	uint64_t   ips[];     /* vector of IPs */
-};
+typedef struct perf_event_header pe_header_t;
+
+typedef struct perf_event_callchain_s {
+  uint64_t    nr;        /* number of IPs */ 
+  uint64_t    ips[];     /* vector of IPs */
+} pe_callchain_t;
+
+typedef struct perf_event_mmap_page pe_mmap_t;
 
 
 
@@ -148,15 +167,28 @@ extern __thread bool hpcrun_thread_suppress_sample;
 // forward declarations 
 //******************************************************************************
 
-void perf_thread_init();
-void perf_thread_fini();
+static bool 
+perf_thread_init();
+
+static void 
+perf_thread_fini();
+
+static cct_node_t *
+perf_add_kernel_callchain(
+  cct_node_t *leaf
+);
+
+static int perf_event_handler(
+  int sig, 
+  siginfo_t* siginfo, 
+  void* context
+);
 
 
 //******************************************************************************
 // local variables
 //******************************************************************************
 
-static int the_signal = SIGIO;
 
 static uint16_t perf_kernel_lm_id;
 
@@ -171,8 +203,14 @@ static long threshold;
 static sigset_t sig_mask;
 
 static int pagesize;
+static size_t tail_mask;
+
 static const char *event_name = "PERF_COUNT_HW_CPU_CYCLES";
 
+static const char * dashes_separator = 
+  "---------------------------------------------------------------------------\n";
+static const char * equals_separator =
+  "===========================================================================\n";
 
 // Special case to make perf init a soft failure.
 // Make sure that we don't use perf if it won't work.
@@ -199,10 +237,8 @@ struct sigevent             __thread sigev;
 struct timespec             __thread real_start; 
 struct timespec             __thread cpu_start; 
 
-int   __thread perf_fd;
-struct perf_event_mmap_page __thread *perf_mmap;
-struct perf_event_callchain __thread *perf_callchain;
-int                         __thread perf_mmap_size;
+int                         __thread perf_thread_fd;
+pe_mmap_t                   __thread *perf_mmap;
 
 
 
@@ -228,19 +264,19 @@ perf_start()
 
   monitor_real_pthread_sigmask(SIG_UNBLOCK, &sig_mask, NULL);
 
-  ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
-  ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0);
+  ioctl(perf_thread_fd, PERF_EVENT_IOC_RESET, 0);
+  ioctl(perf_thread_fd, PERF_EVENT_IOC_ENABLE, 0);
 
-  fcntl(perf_fd, F_SETFL,  O_ASYNC | O_NONBLOCK);
+  fcntl(perf_thread_fd, F_SETFL,  O_ASYNC | O_NONBLOCK);
 
   struct f_owner_ex owner;
   owner.type = F_OWNER_TID;
   owner.pid  = syscall(SYS_gettid);
-  ret = fcntl(perf_fd, F_SETOWN_EX, &owner);
+  ret = fcntl(perf_thread_fd, F_SETOWN_EX, &owner);
 
   if (ret == -1) {
     fprintf(stderr, "can't set fcntl(F_SETOWN_EX) on %d: %s\n", 
-	    perf_fd, strerror(errno));
+	    perf_thread_fd, strerror(errno));
   }
 
   perf_started = 1;
@@ -253,70 +289,139 @@ perf_stop()
   if (!perf_initialized) return;
 
   monitor_real_pthread_sigmask(SIG_BLOCK, &sig_mask, NULL);
-  ioctl(perf_fd, PERF_EVENT_IOC_DISABLE, 0);
+  ioctl(perf_thread_fd, PERF_EVENT_IOC_DISABLE, 0);
 
   perf_started = 0;
 }
 
 
-void
-perf_dump_callchain()
+//----------------------------------------------------------
+// read from perf_events mmap'ed buffer
+//----------------------------------------------------------
+
+static int 
+perf_read(
+  void *buf, 
+  size_t bytes_wanted
+)
 {
-  int print;
+  // front of the circular data buffer
+  char *data = BUFFER_FRONT; 
 
-  char *pc = (char *) perf_callchain;
-  struct perf_event_callchain *sd = 
-    (struct perf_event_callchain *) (pc + (perf_mmap->data_tail % pagesize));
-  int i;
+  // compute bytes available in the circular buffer 
+  size_t bytes_available = perf_mmap->data_head - perf_mmap->data_tail;
 
-  print = (sd->nr > 0);
+  if (bytes_wanted > bytes_available) return -1;
 
-  if (print) {
-    for (i=0; i < sd->nr; i++) {
-      printf("%d 0x%lx\n", myid, sd->ips[i]);
-    }
-    printf("\n");
+  // compute offset of tail in the circular buffer
+  unsigned long tail = BUFFER_OFFSET(perf_mmap->data_tail);
+
+  long bytes_at_right = BUFFER_SIZE - tail;
+
+  // bytes to copy to the right of tail
+  size_t right = bytes_at_right < bytes_wanted ? bytes_at_right : bytes_wanted;
+
+  // copy bytes from tail position
+  memcpy(buf, data + tail, right);
+
+  // if necessary, wrap and continue copy from left edge of buffer
+  if (bytes_wanted > right) {
+    size_t left = bytes_wanted - right;
+    memcpy(buf + right, data, left);
   }
-  perf_mmap->data_head = 0;
-  perf_mmap->data_tail = 0;
+
+  // update tail after consuming bytes_wanted
+  perf_mmap->data_tail += bytes_wanted;
+
+  return 0;
 }
 
 
-bool
+static inline int
+perf_read_header(
+  pe_header_t *hdr
+)
+{
+  return perf_read(hdr, sizeof(pe_header_t));
+}
+
+
+static inline int
+perf_read_uint64_t(
+  uint64_t *val
+)
+{
+  return perf_read(val, sizeof(uint64_t));
+}
+
+
+//----------------------------------------------------------
+// predicates that test perf availability
+//----------------------------------------------------------
+
+static bool
 perf_kernel_syms_avail()
 {
   FILE *ksyms = fopen(HPCRUN_FMT_KERNEL_SYMBOLS, "r");
-  if (ksyms != NULL) {
-    fclose(ksyms);
-    return true;
-  }
-  return false;
+  bool success = (ksyms != NULL);
+  if (success) fclose(ksyms);
+  return success;
 }
 
 
-cct_node_t *
-add_kernel_callchain(cct_node_t *leaf)
+static bool
+perf_event_avail()
 {
-  char *pcc_v = (char *) perf_callchain;
-  struct perf_event_callchain *pcc = 
-    (struct perf_event_callchain *) (pcc_v + (perf_mmap->data_tail % pagesize));
-
-  cct_node_t *parent = leaf;
-  for (int i = pcc->nr - 1; i > 0; i--) {
-    ip_normalized_t npc = { .lm_id = perf_kernel_lm_id, .lm_ip = pcc->ips[i] };
-    cct_addr_t frm = { .ip_norm = npc };
-    cct_node_t *child = hpcrun_cct_insert_addr(parent, &frm);
-    parent = child;
+  static int checked = PERF_EVENT_AVAILABLE_UNKNOWN;
+  
+  switch (checked) {
+  case PERF_EVENT_AVAILABLE_UNKNOWN:
+    {
+      struct stat buf;
+      int rc = stat("/proc/sys/kernel/perf_event_paranoid", &buf);
+      checked = (rc == 0) ? PERF_EVENT_AVAILABLE_YES : PERF_EVENT_AVAILABLE_NO; 
+    }
+  case PERF_EVENT_AVAILABLE_NO:      
+  case PERF_EVENT_AVAILABLE_YES:     
+    break;
   }
-  perf_mmap->data_head = 0;
-  perf_mmap->data_tail = 0;
 
-  return parent;
+  return (checked == PERF_EVENT_AVAILABLE_YES);     
 }
 
 
-void
-perf_attr_init(struct perf_event_attr *attr)
+//----------------------------------------------------------
+// initialization and finalization
+//----------------------------------------------------------
+
+static void 
+perf_init()
+{
+  perf_ksyms_avail = perf_kernel_syms_avail();
+
+  pagesize = sysconf(_SC_PAGESIZE);
+  tail_mask = PERF_TAIL_MASK(pagesize);
+
+  // initialize mask to block PERF_SIGNAL 
+  sigemptyset(&sig_mask);
+  sigaddset(&sig_mask, PERF_SIGNAL);
+
+  // if kernel symbols are available, we will attempt to collect kernel 
+  // callchains and add them to our call paths 
+  if (perf_ksyms_avail) {
+    hpcrun_kernel_callpath_register(perf_add_kernel_callchain);
+    perf_kernel_lm_id = 
+      hpcrun_loadModule_add(HPCRUN_FMT_KERNEL);
+  }
+ 
+  monitor_sigaction(PERF_SIGNAL, &perf_event_handler, 0, NULL);
+}
+
+
+static void
+perf_attr_init(
+  struct perf_event_attr *attr
+)
 {
   memset(attr, 0, sizeof(struct perf_event_attr));
 
@@ -341,96 +446,157 @@ perf_attr_init(struct perf_event_attr *attr)
 }
 
 
-void
+static bool
 perf_thread_init()
 {
   if (perf_thread_initialized == 0) {
     struct perf_event_attr attr;
     perf_attr_init(&attr);
-    perf_fd = perf_event_open(&attr, THREAD_SELF, CPU_ANY, 
+    perf_thread_fd = perf_event_open(&attr, THREAD_SELF, CPU_ANY, 
 			      GROUP_FD, PERF_FLAGS);
 
-    perf_mmap_size = pagesize * 2;
-    void *map_result = mmap(NULL, perf_mmap_size, PROT_WRITE | PROT_READ, 
-			    MAP_SHARED, perf_fd, MMAP_OFFSET_0);
+    void *map_result = 
+      mmap(NULL, PERF_MMAP_SIZE(pagesize), PROT_WRITE | PROT_READ, 
+	   MAP_SHARED, perf_thread_fd, MMAP_OFFSET_0);
 
     if (map_result == MAP_FAILED) {
       EMSG("Linux perf mmap failed: %s", strerror(errno));
+      return false;
     }
 
-    char *mm = (char *) map_result;
-
-    perf_mmap  = (struct perf_event_mmap_page *) mm;
-    perf_callchain = (struct perf_event_callchain *) (mm + pagesize);
+    perf_mmap  = (pe_mmap_t *) map_result;
 
     if (perf_mmap) {
-      memset(perf_mmap, 0, sizeof(struct perf_event_mmap_page));
+      memset(perf_mmap, 0, sizeof(pe_mmap_t));
       perf_mmap->version = 0; 
       perf_mmap->compat_version = 0; 
       perf_mmap->data_head = 0; 
       perf_mmap->data_tail = 0; 
     }
+
     perf_thread_initialized = 1;
   }
+  return true;
 }
 
 
 void
 perf_thread_fini()
 {
-  munmap(perf_mmap, perf_mmap_size);
-  close(perf_fd);
+  if (perf_thread_initialized) {
+    munmap(perf_mmap, PERF_MMAP_SIZE(pagesize));
+    close(perf_thread_fd);
+  }
   perf_thread_initialized = 0;
 }
 
 
-static int
-perf_event_handler(int sig, siginfo_t* siginfo, void* context)
-{
-  void *pc = 0; // FIXME
+//----------------------------------------------------------
+// signal handling and processing of kernel callchains
+//----------------------------------------------------------
 
-  ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
+// extend a user-mode callchain with kernel frames (if any)
+static cct_node_t *
+perf_add_kernel_callchain(
+  cct_node_t *leaf
+)
+{
+  cct_node_t *parent = leaf;
+  pe_header_t hdr; 
+
+  if (perf_read_header(&hdr) == 0) {
+    if (hdr.type == PERF_RECORD_SAMPLE) {
+      uint64_t n_frames;
+      // determine how many frames in the call chain 
+      if (perf_read_uint64_t(&n_frames) == 0) {
+
+	if (n_frames > 0) {
+	  // allocate space to receive IPs for kernel callchain 
+	  uint64_t *ips = alloca(n_frames * sizeof(uint64_t));
+
+	  // read the IPs for the frames 
+	  if (perf_read(ips, n_frames * sizeof(uint64_t)) == 0) {
+
+	    // add kernel IPs to the call chain top down, which is the 
+	    // reverse of the order in which they appear in ips
+	    for (int i = n_frames - 1; i > 0; i--) {
+	      ip_normalized_t npc = 
+		{ .lm_id = perf_kernel_lm_id, .lm_ip = ips[i] };
+	      cct_addr_t frm = { .ip_norm = npc };
+	      cct_node_t *child = hpcrun_cct_insert_addr(parent, &frm);
+	      parent = child;
+	    }
+	  } else {
+	    TMSG(LINUX_PERF, "unable to read all frames on fd %d", 
+		 perf_thread_fd);
+	  }
+	}
+      } else {
+	TMSG(LINUX_PERF, "unable to read number of frames on fd %d", 
+	     perf_thread_fd);
+      }
+    }
+  }
+
+  return parent;
+}
+
+
+static int
+perf_event_handler(
+  int sig, 
+  siginfo_t* siginfo, 
+  void* context
+)
+{
+  if (siginfo->si_code < 0) {
+    // signal not generated by kernel for profiling
+    TMSG(LINUX_PERF, "signal si_code %d >= 0 indicates not from kernel", 
+	 siginfo->si_code);
+    return 1; // tell monitor the signal has not been handled.
+  }
+
+#if 0
+  if (siginfo->si_code != POLL_HUP) {
+    // expect POLL_HUP and not POLL_IN
+    TMSG(LINUX_PERF, "signal for si_code %d is not POLL_HUP", siginfo->si_code);
+    return 1; // tell monitor the signal has not been handled.
+  }
+
+  if (siginfo->si_fd != perf_thread_fd) {
+    TMSG(LINUX_PERF, "signal for fd %d is not for perf_thread_fd %d", 
+	 siginfo->si_fd, perf_thread_fd);
+    return 1; // tell monitor the signal has not been handled.
+  }
+#endif
+
+  void *pc = hpcrun_context_pc(context); 
 
   // if sampling disabled explicitly for this thread, skip all processing
-  if (hpcrun_thread_suppress_sample) 
+  if (hpcrun_thread_suppress_sample) {
     return 0; // tell monitor the signal has been handled.
+  }
 
-  // If the interrupt came from inside our code, then drop the sample
-  // and return and avoid any MSG.
+  // if the interrupt came while inside our code, then drop the sample
+  // and return and avoid the potential for deadlock.
   if (! hpcrun_safe_enter_async(pc)) {
     hpcrun_stats_num_samples_blocked_async_inc();
     return 0; // tell monitor the signal has been handled.
   }
 
-  // perf_dump_callchain();
-
   sample_val_t sv = hpcrun_sample_callpath(context, metric_id, 1,
 					   0/*skipInner*/, 0/*isSync*/);
-
-  // add_kernel_callchain(sv.sample_node);
 
   blame_shift_apply(metric_id, sv.sample_node, 1 /*metricIncr*/);
 
   hpcrun_safe_exit();
-  return 0; // tell monitor the signal has been handled.
-}
 
-
-static void 
-perf_init()
-{
-  perf_ksyms_avail = perf_kernel_syms_avail();
-  pagesize = sysconf(_SC_PAGESIZE);
-  sigemptyset(&sig_mask);
-  sigaddset(&sig_mask, the_signal);
-
-  if (perf_ksyms_avail) {
-    hpcrun_kernel_callpath_register(add_kernel_callchain);
-    perf_kernel_lm_id = 
-      hpcrun_loadModule_add(HPCRUN_FMT_KERNEL);
+  int rc = ioctl(perf_thread_fd, PERF_EVENT_IOC_REFRESH, 1);
+  if (rc == -1) {
+    TMSG(LINUX_PERF, "error in IOC_REFRESH");
   }
- 
-  monitor_sigaction(the_signal, &perf_event_handler, 0, NULL);
+
+  return 0; // tell monitor the signal has been handled.
 }
 
 
@@ -570,21 +736,29 @@ METHOD_FN(process_event_list, int lush_metrics)
 
   if (perf_unavail) { return; }
 
-#if 0
   char* evlist = METHOD_CALL(self, get_event_str);
-
-  char *event;
+#if 1
+  char *event = start_tok(evlist); 
+  char name[1024];
+  hpcrun_extract_ev_thresh(event, sizeof(name), name, &threshold, 
+			   DEFAULT_THRESHOLD);
+#else
+  NEEDS WORK FOR MULTIPLE EVENTS
   for (event = start_tok(evlist); more_tok(); event = next_tok()) {
     char name[1024];
 
     TMSG(LINUX_PERF,"checking event spec = %s",event);
 
-    hpcrun_extract_ev_thresh(event, sizeof(name), name, &threshold, DEFAULT_THRESHOLD);
+    hpcrun_extract_ev_thresh(event, sizeof(name), name, &threshold, 
+			     DEFAULT_THRESHOLD);
 
   }
 #endif
+
+#if 0 
   char *name = "PERF_COUNT_HW_CPU_CYCLES";
   threshold = DEFAULT_THRESHOLD;
+#endif
 
   perf_initialized = true;
   perf_init();
@@ -592,7 +766,7 @@ METHOD_FN(process_event_list, int lush_metrics)
 
   metric_id = hpcrun_new_metric();
 
-  hpcrun_set_metric_info_and_period(metric_id, name,
+  hpcrun_set_metric_info_and_period(metric_id, strdup(name),
 				    MetricFlags_ValFmt_Int,
 				    threshold, prop);
 }
@@ -607,14 +781,19 @@ METHOD_FN(gen_event_set, int lush_metrics)
 static void
 METHOD_FN(display_events)
 {
-  printf("===========================================================================\n");
-  printf("Available Linux perf events\n");
-  printf("===========================================================================\n");
-  printf("Name\t\tDescription\n");
-  printf("---------------------------------------------------------------------------\n");
-  printf("%s\tTotal cycles.\n", 
-         "PERF_COUNT_HW_CPU_CYCLES");
-  printf("\n");
+  if (perf_event_avail()) {
+
+    printf(equals_separator);
+    printf("Available Linux perf events\n");
+    printf(equals_separator);
+
+    printf("Name\t\tDescription\n");
+    printf(dashes_separator);
+
+    printf("%s\tTotal cycles.\n", 
+	   "PERF_COUNT_HW_CPU_CYCLES");
+    printf("\n");
+  }
 }
 
 
