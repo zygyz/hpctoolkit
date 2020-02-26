ec64225e3a79639770199ebecd81f9bc34a0b6ee
Yizi Gu <yizigu@gmail.com>
2020-02-26 16:06:50 -0600

    Revert "[dyninst-unwind][debug] print return address"
    This reverts commit 4ee5e1c72eac8bf88ae1d95c698a937ff0cbbce3.
    
    Conflicts:
    	src/tool/hpcrun/unwind/x86-family/x86-unwind.c

--- src/tool/hpcrun/unwind/x86-family/x86-unwind.c
+++ src/tool/hpcrun/unwind/x86-family/x86-unwind.c
@@ -717,7 +717,6 @@ unwind_dyninst_frame(hpcrun_unw_cursor_t* cursor)
       return false; 
     }
     ra_loc = (void**)*(bp + DYNINST_USER_FRAME_SP_OFFSET);
-    EEMSG("locator based %p return addr: %p", ra_loc, *ra_loc);
   } else {
     // try frame pointer based unwinding, in this case, return address is one
     // word above frame pointer address 
